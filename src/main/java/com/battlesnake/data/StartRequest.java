526604ff084542f19f5f2660ec37918d1c20fe27
DAChenAlt <74833667+DAChenAlt@users.noreply.github.com>
2021-01-26 15:28:44 -0600

    Update StartRequest.java

--- src/main/java/com/battlesnake/data/StartRequest.java
+++ src/main/java/com/battlesnake/data/StartRequest.java
@@ -22,33 +22,9 @@ public class StartRequest {
   private String gameId; // UUID	 
   private int width; //integer	 
   private int height; //integer	 
+  private String apiversion;
 
   public StartRequest() {
-  }
-
-  @JsonProperty("game_id")
-  public String getGameId() {
-    return this.gameId;
-  }
-
-  public int getWidth() {
-    return this.width;
-  }
-
-  public int getHeight() {
-    return this.height;
-  }
-
-  public void setGameId(String gameId) {
-    this.gameId = gameId;
-  }
-
-  public void setWidth(int width) {
-    this.width = width;
-  }
-
-  public void setHeight(int height) {
-    this.height = height;
   }
 
 }
